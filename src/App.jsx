212a87ea8288b8e713d3f98d399ffbee7717264d
JC-Coder <josephchimezie2003@gmail.com>
2023-01-29 14:52:11 +0100

    refactor routers

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<BookEscort />} />
+          <Route path="/book-escort" element={<BookEscort />} />
         </Routes>
       </Router>
   )
